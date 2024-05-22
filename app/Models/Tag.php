4c2df6a92a49ad79024be65bad883b13fa9955c4
Nour-Cherrat <cherrat.nour@gmail.com>
2024-05-22 02:41:19 +0100

    Update file

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -9,6 +9,11 @@ class Tag extends Model
 {
     use HasFactory;
 
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
     public function budgets()
     {
         return $this->hasMany(Budget::class);
